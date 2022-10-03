ea71d2d3e3cc877da3405d253c1ea21307d93fa4
Soumadittya Ghosh <soumadittya@Soumadittyas-MacBook-Pro.local>
2022-10-03 10:36:04 +0530

    server.js file updated 3

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World with Soumadittya Ghosh. Update_2....');
+  res.send('Hello World with Soumadittya Ghosh. Update_3....');
 });
 
 app.listen(PORT, HOST);
